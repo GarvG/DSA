d051aa922abcf1bd2e4a8540e337fe7e59f95981
GarvG <67434601+GarvG@users.noreply.github.com>
2022-10-25 18:23:15 +0530

    DAILY PRATICE QUESTIONS

---
+++ src/HashMap/Pratice.java
@@ -0,0 +1,13 @@
+package HashMap;
+
+public class Pratice {
+    public static void main(String[] args) {
+        // put -> update IF ALREADY EXISTS OR CREATE A NEW
+        // get -> returns value or null
+        // contains key -> IF exists true or false
+        // remove -> IF exists return value or else null
+        // Keyset -> return all keys
+        // size -> return all size;
+
+    }
+}
