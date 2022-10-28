960cb69734ccfb2bae9fc046dcd642c6ecd2c942
GarvG <67434601+GarvG@users.noreply.github.com>
2022-10-28 23:21:23 +0530

    DAILY PRATICE QUESTIONS

---
+++ src/Binary_Search/FINDING_PIVOT_IN_ROTATED_ARRAY.java
@@ -0,0 +1,34 @@
+package Binary_Search;
+
+public class FINDING_PIVOT_IN_ROTATED_ARRAY {
+    public static void main(String[] args) {
+        int[]arr={15,18,2,3,6,12};
+        int ans=findPivot(arr);
+        System.out.println(ans);
+    }
+    static int findPivot(int[] arr)
+    {
+        int n=arr.length;
+        int start=0;
+        int end=n-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            int next=(mid+1)%n;
+            int prev=(mid-1+n)%n;
+            if(arr[mid]<=arr[prev]&&arr[mid]<=arr[next])
+            {
+                return mid;
+            }
+            else if(arr[mid]<=arr[end])
+            {
+                end=mid-1;
+            }
+            else if(arr[mid]>=arr[start])
+            {
+                start=mid+1;
+            }
+        }
+        return 0;
+    }
+}
