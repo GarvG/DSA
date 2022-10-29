309d0f680762bb007bbb07c917f462522a844030
GarvG <67434601+GarvG@users.noreply.github.com>
2022-10-29 19:10:40 +0530

    DAILY PRATICE QUESTIONS

--- src/Binary_Search/FINDING_PIVOT_IN_ROTATED_ARRAY.java
+++ src/Binary_Search/FINDING_PIVOT_IN_ROTATED_ARRAY.java
@@ -2,7 +2,7 @@ package Binary_Search;
 
 public class FINDING_PIVOT_IN_ROTATED_ARRAY {
     public static void main(String[] args) {
-        int[]arr={15,18,2,3,6,12};
+        int[]arr={3,2};
         int ans=findPivot(arr);
         System.out.println(ans);
     }
@@ -20,13 +20,14 @@ public class FINDING_PIVOT_IN_ROTATED_ARRAY {
             {
                 return mid;
             }
-            else if(arr[mid]<=arr[end])
-            {
-                end=mid-1;
-            }
+
             else if(arr[mid]>=arr[start])
             {
                 start=mid+1;
+            }
+            else if(arr[mid]<=arr[end])
+            {
+                end=mid-1;
             }
         }
         return 0;
---
+++ src/Binary_Search/RBS.java
@@ -0,0 +1,75 @@
+package Binary_Search;
+
+public class RBS {
+    public static void main(String[] args) {
+        int[] arr = {6,7,1,2,3,4,5};
+        System.out.println(pivot(arr));
+    }
+    static int search(int[]nums,int target)
+    {
+        int pivot=pivot(nums);
+      if(pivot==-1)
+      {
+          return Search(nums,target,0,nums.length-1);
+      }
+      if(nums[pivot]==target)
+      {
+          return target;
+      }
+    if(target>=nums[0])
+    {
+        return Search(nums,target,0,pivot-1);
+    }
+    else{
+        return Search(nums,target,pivot+1, nums.length-1);
+    }
+    }
+    static int Search(int [] arr,int target,int start,int end)
+    {
+        int len=arr.length;
+
+        while(start<=end)
+        {
+            //  int middle=(start+end)/2;
+            int middle=start+(end-start)/2;
+            if(arr[middle]==target)
+            {
+                return middle;
+            }
+            if(target>arr[middle])
+            {
+                start=middle+1;
+            }
+            else{
+                end=middle-1;
+            }
+
+        }
+        return -1;
+    }
+    static int pivot(int[] arr)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1])
+            {
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start])
+            {
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
