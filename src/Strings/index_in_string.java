43cc8a74e226f6d00488b6d7984cbce0e0730b76
GarvG <67434601+GarvG@users.noreply.github.com>
2022-11-10 23:05:41 +0530

    DAILY PRATICE QUESTIONS(STRINGS)

---
+++ src/Strings/index_in_string.java
@@ -0,0 +1,10 @@
+package Strings;
+
+class index_in_string {
+    public static void main(String[] args) {
+
+    }
+    public int strStr(String haystack, String needle) {
+        return  -1;
+    }
+}
