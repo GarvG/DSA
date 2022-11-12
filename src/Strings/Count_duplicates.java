772409a81a3636eeef3e0918dd722cf9345a81c2
GarvG <67434601+GarvG@users.noreply.github.com>
2022-11-12 17:21:56 +0530

    DAILY PRATICE QUESTIONS(STRINGS)

---
+++ src/Strings/Count_duplicates.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class Count_duplicates {
+    public static void main(String[] args) {
+        String ans="test string";
+        System.out.println(duplicates(ans));
+    }
+    static int duplicates (String S)
+    {
+        String ans="";
+        int []count=new int[256];
+        for(int i=0;i<S.length();i++)
+        {
+            int index=S.charAt(i);
+            System.out.println(index);
+            count[index]++;
+        }
+        System.out.println(Arrays.toString(count));
+
+for(int i=0;i<count.length;i++)
+{
+    if(count[i]>1)
+    {
+     ans+=(char)(i);
+    }
+}
+        System.out.println(ans);
+            return -1;
+
+    }
+}
