61c4ee32e4866bfb5032567b6b606f1c3135c9ad
GarvG <67434601+GarvG@users.noreply.github.com>
2022-11-24 23:50:23 +0530

    Interview Question Revise

--- src/test.java
+++ src/test.java
@@ -1,19 +1,52 @@
 import java.util.*;
 public class test {
-    public static void main(String[] args) {
-        int []nums={1,2,4,5,2};
-        ArrayList<Integer> hs=new ArrayList<Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-//            System.out.print("before"+hs);
-            if(hs.contains(nums[i]))
-            {
-                hs.remove(Integer.valueOf(nums[i]));
-            }
-            else{
-                hs.add(nums[i]);
-            }
+    private int i=1;
+    public  class  A{
+        static {
+            System.out.println("static");
         }
-        System.out.print(hs);
+        {
+            System.out.println("block");
+        }
+
+    }
+//    public A()
+//    {
+//        System.out.println("A");
+//    }
+long var;
+    public void test(long param){
+        var=param;
+    }
+
+
+
+    public static void main(String[] args) {
+//        int []nums={1,2,4,5,2};
+//        ArrayList<Integer> hs=new ArrayList<Integer>();
+//        for(int i=0;i<nums.length;i++)
+//        {
+////            System.out.print("before"+hs);
+//            if(hs.contains(nums[i]))
+//            {
+//                hs.remove(Integer.valueOf(nums[i]));
+//            }
+//            else{
+//                hs.add(nums[i]);
+//            }
+//        }
+     //   A a=new A();
+        //System.out.print(hs);
+//        int number=11;
+//        int NUMBER=22;
+//        int Number=33;
+//        System.out.print(number + " ");
+//        System.out.print(NUMBER + " ");
+//        System.out.println(Number+" ");
+    test a,b;
+    a=new test();
+   // b=new test(5);
+
+
     }
 }
